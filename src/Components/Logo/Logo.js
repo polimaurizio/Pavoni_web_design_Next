0217cfd7d5fde162618dbdf2c93bee710572920e
polimaurizio <mauriziopoli.02@gmail.com>
2023-06-30 10:29:43 +0200

    refactor Logo add variants

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React from "react";
 
-const Logo = ({}) => {
+const LogoVariants = {
+  primary: "Primary",
+  secondary: "Secondary",
+};
+
+const Logo = ({ variant }) => {
+  if (variant === LogoVariants.primary) {
+    return (
+      <img
+        width="130px"
+        height="auto"
+        src="/Loghi/logo_pavoni.png"
+        alt="logo"
+      />
+    );
+  }
+
   return (
-    <img width="130px" height="auto" src="/Loghi/logo_pavoni.png" alt="logo" />
+    <img
+      width="130px"
+      height="auto"
+      src="/Loghi/logo_pavoni_white.png"
+      alt="logo"
+    />
   );
 };
 
