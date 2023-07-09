b31a55f77654914fdfaa0e8fbc7965acb4f3f53d
polimaurizio <mauriziopoli.02@gmail.com>
2023-07-09 18:10:14 +0200

    add correct content

--- src/Components/NewProduct/NewProduct.js
+++ src/Components/NewProduct/NewProduct.js
@@ -4,19 +4,16 @@ import CustomButton from "../Button/Button";
 export const NewProduct = ({}) => {
   return (
     <div className="grid grid-cols-12 gap-5 pt-24">
-      <div className="col-start-1 col-end-6 hidden md:block">
-        <img src="/Img/img_1.jpg" alt="caffe_machine" />
+      <div className="col-start-1 col-end-7 hidden md:block">
+        <img src="/Img/macina_caffe_1.jpeg" alt="caffe_machine mirror_image" />
       </div>
       <div className="pl-10 my-auto align-bottom col-start-1 md:col-start-7 col-end-12 font-light font-montserrat">
         <h1 className="text-[50px] sm:text-[70px] mb-10 md:mb-16 text-primary">
           Nuovi prodotti
         </h1>
         <p className="leading-7 text-primary">
-          Dalla metà degli anni Ottanta l’Azienda si è impegnata nello studio di
-          nuove soluzioni ed alla realizzazione di pannellature in materiali
-          termoplastici, che consentono un notevole snellimento del ciclo
-          produttivo. All’ergonomia è stata dedicata una particolare
-          attenzione...
+          Il macinacaffè ha un contenitore per caffè in grani con capacità di
+          250 gr. e consente un’ampia regolazione della macinatura del caffè.
         </p>
         <CustomButton href={"#"} className={"pt-14 md:pt-10"}>
           Scopri di più
