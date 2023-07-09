650471ddcf369a66beb2ff69c032cc95cc9e778e
polimaurizio <mauriziopoli.02@gmail.com>
2023-07-09 15:04:33 +0200

    fix Card width problem

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -54,38 +54,36 @@ export const Card = ({}) => {
       <h1 className="col-start-2 col-end-12 text-[50px] sm:text-[70px] mb-10 md:mb-16 font-light font-montserrat text-primary">
         I nostri prodotti
       </h1>
-      <motion.div
-        ref={ref}
-        style={{ x }}
-        className="w-full h-min pl-[9%] col-start-1 col-end-13 no-scrollbar overflow-x-scroll"
-      >
-        <div className="flex">
-          {cardContent.map((content, index) => (
-            <div
-              key={index}
-              className="bg-[#f9f9f9] h-min min-w-[250px] w-[280px] mr-10 rounded-md border border-primary border-opacity-5"
-            >
-              <img
-                src={content.img}
-                alt={"products_" + index}
-                className="rounded-tl-md rounded-tr-md"
-              />
-              <div className="p-5 font-montserrat text-primary">
-                <h1 className="font-semibold">{content.title}</h1>
-                <p className="pt-2 text-xs">{content.text}</p>
-                <div className="flex justify-end pt-3">
-                  <button
-                    className="py-2 px-4 text-xs h-min bg-[#f2f2f2] hover:bg-primary hover:text-white rounded-full"
-                    href="#"
-                  >
-                    Aggiungi
-                  </button>
+      <div className="w-full h-min pl-[9%] col-start-1 col-end-13 no-scrollbar overflow-x-scroll">
+        <motion.div ref={ref} style={{ x }}>
+          <div className="flex">
+            {cardContent.map((content, index) => (
+              <div
+                key={index}
+                className="bg-[#f9f9f9] h-min min-w-[250px] w-[280px] mr-10 rounded-md border border-primary border-opacity-5"
+              >
+                <img
+                  src={content.img}
+                  alt={"products_" + index}
+                  className="rounded-tl-md rounded-tr-md"
+                />
+                <div className="p-5 font-montserrat text-primary">
+                  <h1 className="font-semibold">{content.title}</h1>
+                  <p className="pt-2 text-xs">{content.text}</p>
+                  <div className="flex justify-end pt-3">
+                    <button
+                      className="py-2 px-4 text-xs h-min bg-[#f2f2f2] hover:bg-primary hover:text-white rounded-full"
+                      href="#"
+                    >
+                      Aggiungi
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
-      </motion.div>
+            ))}
+          </div>
+        </motion.div>
+      </div>
     </div>
   );
 };
